959ac48810cdaff6cf1c29d439fc706066903a9f
hasanshaik <hasanshaik0393@gmail.com>
2016-11-02 14:08:27 +0530

    created app.java

---
+++ Non_Maven_JavaProject/src/main/java/App.java
@@ -0,0 +1,23 @@
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+	public int calculateSomething() {
+		return 0;
+	}
+    
+    public int givesThree(String vari){
+        if(vari.equals("three")){
+            return 3;
+        }else{
+            return 0;
+        }
+    }
+}
